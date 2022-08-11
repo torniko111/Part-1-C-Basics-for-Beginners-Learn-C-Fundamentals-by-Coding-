1770568509124fadc116e43f1e07bf1b7e2a2fb5
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-08-12 02:11:16 +0400

    First exercise

---
+++ HamedanyExercices/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("please enter a number from 1 to 10");
+
+int userInput = int.Parse(Console.ReadLine());
+
+if(userInput >0 && userInput < 10)
+{
+    Console.WriteLine("the number is valid");
+}
+else
+{
+    Console.WriteLine("the number is not valid");
+}
